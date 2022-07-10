4cfaf0ac67f2140e649a207748c936db50b9389f
harryhare <harryhare@163.com>
2022-07-11 03:10:18 +0800

    update 12.21 solution

--- 12_concurrent/hw/p.21.c
+++ 12_concurrent/hw/p.21.c
@@ -1,35 +1,64 @@
+
 #include "csapp.h"
 
+#define N 10
 
-int readcnt;    /* Initially = 0 */
-sem_t mutex, w; /* Both initially = 1 */
+/* Global variables */
+sem_t r;    /* Initially = 1 */
+sem_t w;    /* Initially = 1 */
+
+sem_t m_r_counter; /* Initially = 0 */
+sem_t m_w_counter; /* Initially = 0 */
+
+int write_count = 0;
+int read_count = 0;
 
 void reader(void) {
     while (1) {
-        P(&mutex);
-        readcnt++;
-        if (readcnt == 1)
+        P(&r);
+        P(&m_r_counter);
+        V(&r);
+        read_count++;
+        if (read_count == 1)
             P(&w);
-        V(&mutex);
+        V(&m_r_counter);
 
-        /* Critical section */
-        /* Reading happens  */
+        /* Critical section: */
+        /* Reading happens   */
 
-        P(&mutex);
-        readcnt--;
-        if (readcnt == 0)
+        P(&m_r_counter);
+        read_count--;
+        if (read_count == 0)
             V(&w);
-        V(&mutex);
+        V(&m_r_counter);
     }
 }
 
 void writer(void) {
+    int i;
+
     while (1) {
+        P(&m_w_counter);
+        write_count++;
+        if (write_count == 1) {
+            P(&r);
+        }
+        V(&m_w_counter);
+
         P(&w);
 
-        /* Critical section */
-        /* Writing happens  */
+        /* Critical section: */
+        /* Writing happens   */
 
         V(&w);
+
+        P(&m_w_counter);
+        if (write_count == 0) {
+            V(&r);
+        }
+        V(&m_w_counter);
     }
 }
+
+/* https://blog.csdn.net/lixufeng1992/article/details/13615189
+ */
