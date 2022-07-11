5c33d0b923319a07219086fd7812df10ea2a5ca2
harryhare <harryhare@163.com>
2022-07-12 02:38:32 +0800

    update

--- 12_concurrent/hw/p.21.c
+++ 12_concurrent/hw/p.21.c
@@ -6,23 +6,25 @@
 /* Global variables */
 sem_t r;    /* Initially = 1 */
 sem_t w;    /* Initially = 1 */
+sem_t rg;    /* Initially = 1 */
 
-sem_t m_r_counter; /* Initially = 0 */
-sem_t m_w_counter; /* Initially = 0 */
+sem_t m_r_counter; /* Initially = 1 */
+sem_t m_w_counter; /* Initially = 1 */
 
 int write_count = 0;
 int read_count = 0;
 
 void reader(void) {
     while (1) {
+        P(&rg);
         P(&r);
-        P(&m_r_counter);//
-        V(&r);  // 这句和上面那句颠倒好像也行
+        P(&m_r_counter);
         read_count++;
         if (read_count == 1)
             P(&w);
         V(&m_r_counter);
-
+        V(&r);
+        V(&rg);
         /* Critical section: */
         /* Reading happens   */
 
@@ -60,6 +62,11 @@ void writer(void) {
     }
 }
 
-/* https://blog.csdn.net/lixufeng1992/article/details/13615189
+/* offical solution:
+ * Concurrent Control with Readers and Writers CACM, Oct, 1971
+ * https://dl.acm.org/doi/pdf/10.1145/362759.362813
+ *
+ * other solution(seems wrong):
+ * https://blog.csdn.net/lixufeng1992/article/details/13615189
  * https://blog.csdn.net/qq_46311811/article/details/122354513
  */
