16e6e8e4a9205cbdcabd0838290378e67aa4ab04
harryhare <harryhare@163.com>
2022-10-26 10:39:42 +0800

    add chapter 6&7

--- 06_mem/mountain/mountain.c
+++ 06_mem/mountain/mountain.c
@@ -8,7 +8,7 @@
 #define MINBYTES (1 << 14)  /* First working set size */
 #define MAXBYTES (1 << 27)  /* Last working set size */
 #define MAXSTRIDE 15        /* Stride x8 bytes */
-#define MAXELEMS MAXBYTES/sizeof(long) 
+#define MAXELEMS MAXBYTES/sizeof(long)
 
 /* $begin mountainfuns */
 long data[MAXELEMS];      /* The global array we'll be traversing */
@@ -16,12 +16,13 @@ long data[MAXELEMS];      /* The global array we'll be traversing */
 /* $end mountainfuns */
 /* $end mountainmain */
 void init_data(long *data, int n);
+
 int test(int elems, int stride);
+
 double run(int size, int stride, double Mhz);
 
 /* $begin mountainmain */
-int main()
-{
+int main() {
     int size;        /* Working set size (in bytes) */
     int stride;      /* Stride (in array elements) */
     double Mhz;      /* Clock frequency */
@@ -35,60 +36,63 @@ int main()
 
     printf("\t");
     for (stride = 1; stride <= MAXSTRIDE; stride++)
-	printf("s%d\t", stride);
+        printf("s%d\t", stride);
     printf("\n");
 
- /* $begin mountainmain */
-    for (size = MAXBYTES; size >= MINBYTES; size >>= 1) {
+    /* $begin mountainmain */
+    for (size = MAXBYTES; size >= MINBYTES;) {
 /* $end mountainmain */
-	/* Not shown in the text */
-	if (size > (1 << 20))
-	    printf("%dm\t", size / (1 << 20));
-	else
-	    printf("%dk\t", size / 1024);
+        /* Not shown in the text */
+        if (size > (1 << 20))
+            printf("%dm\t", size / (1 << 20));
+        else
+            printf("%dk\t", size / 1024);
 
 /* $begin mountainmain */
-	for (stride = 1; stride <= MAXSTRIDE; stride++) {
-	    printf("%.0f\t", run(size, stride, Mhz));
-	    
-	}
-	printf("\n");
+        for (stride = 1; stride <= MAXSTRIDE; stride++) {
+            printf("%.0f\t", run(size, stride, Mhz));
+
+        }
+        printf("\n");
+        if (size <= 2*(1 << 20)||size>((1l<<20)*16)) {
+            size >>= 1;
+        } else {
+            size -= 1l << 21;
+        }
     }
     exit(0);
 }
 /* $end mountainmain */
 
 /* init_data - initializes the array */
-void init_data(long *data, int n)
-{
+void init_data(long *data, int n) {
     int i;
 
     for (i = 0; i < n; i++)
-	data[i] = i;
+        data[i] = i;
 }
 
 /* $begin mountainfuns */
 /* test - Iterate over first "elems" elements of array "data" with
  *        stride of "stride", using 4x4 loop unrolling.
  */
-int test(int elems, int stride)
-{
-    long i, sx2 = stride*2, sx3 = stride*3, sx4 = stride*4;
+int test(int elems, int stride) {
+    long i, sx2 = stride * 2, sx3 = stride * 3, sx4 = stride * 4;
     long acc0 = 0, acc1 = 0, acc2 = 0, acc3 = 0;
     long length = elems;
     long limit = length - sx4;
 
     /* Combine 4 elements at a time */
     for (i = 0; i < limit; i += sx4) {
-	acc0 = acc0 + data[i];     
-        acc1 = acc1 + data[i+stride];
-	acc2 = acc2 + data[i+sx2]; 
-        acc3 = acc3 + data[i+sx3];
+        acc0 = acc0 + data[i];
+        acc1 = acc1 + data[i + stride];
+        acc2 = acc2 + data[i + sx2];
+        acc3 = acc3 + data[i + sx3];
     }
 
     /* Finish any remaining elements */
     for (; i < length; i += stride) {
-	acc0 = acc0 + data[i];
+        acc0 = acc0 + data[i];
     }
     return ((acc0 + acc1) + (acc2 + acc3));
 }
@@ -97,10 +101,9 @@ int test(int elems, int stride)
  *       "size" is in bytes, "stride" is in array elements, and Mhz is
  *       CPU clock frequency in Mhz.
  */
-double run(int size, int stride, double Mhz)
-{   
+double run(int size, int stride, double Mhz) {
     double cycles;
-    int elems = size / sizeof(double);       
+    int elems = size / sizeof(double);
 
     test(elems, stride);                     /* Warm up the cache */       //line:mem:warmup
     cycles = fcyc2(test, elems, stride, 0);  /* Call test(elems,stride) */ //line:mem:fcyc
---
+++ 06_mem/note.md
@@ -0,0 +1,8 @@
+## homework 34
+隐约能看出点意思,没有书中那么明显, 尤其是 l2 和 l3 的分别
+
+![](../assets/csapp_mem.png)
+![](../assets/csapp_mem2.png)
+
+
+
